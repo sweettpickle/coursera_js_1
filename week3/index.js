f9325a898d1b502b711431a111f62c4cba864300
sweettpickle <sweettpickle@gmail.com>
2019-12-02 21:17:34 +0300

    modif week3

--- week3/index.js
+++ week3/index.js
@@ -3,44 +3,111 @@
  * @returns {Object}
  */
 // module.exports = function (date) {
+// 	let data = new Date(date);
 //
+// 	Object.defineProperties(data, {
+// 		'add': {
+// 			value: function (num, param) {
+// 				if (param === 'years')
+// 					data.setFullYear(data.getFullYear() + num);
+// 				if (param === 'months')
+// 					data.setMonth(data.getMonth() + num);
+// 				if (param === 'days')
+// 					data.setDate(data.getDate() + num);
+// 				if (param === 'hours')
+// 					data.setHours(data.getHours() + num);
+// 				if (param === 'minutes')
+// 					data.setMinutes(data.getMinutes() + num);
+// 				return data;
+// 			}
+// 		},
+// 		'subtract': {
+// 			value: function (num, param) {
+// 				if (param === 'years')
+// 					data.setFullYear(data.getFullYear() - num);
+// 				if (param === 'months')
+// 					data.setMonth(data.getMonth() - num);
+// 				if (param === 'days')
+// 					data.setDate(data.getDate() - num);
+// 				if (param === 'hours')
+// 					data.setHours(data.getHours() - num);
+// 				if (param === 'minutes')
+// 					data.setMinutes(data.getMinutes() - num);
+// 				return data;
+// 			}
+// 		},
+// 		'value': {
+// 			value: data.toLocaleDateString("ru").split(".").reverse().join("-") +
+// 					" " + data.toTimeString().split(" ")[0].slice(0,5)
+// 		}
+// 	});
+// 	return data;
 // };
 
 function Time(str)
 {
-	let params = str.split(" ");
-	let param1 = params[0].split("-");
-	let param2 = params[1].split(":");
-	// console.log(param2);
+	let data = new Date(str);
+	// console.log(data.value);
+	// let params = str.split(" ");
+	// let param1 = params[0].split("-");
+	// let param2 = params[1].split(":");
+	//
 	// let obj = {
-	// 	getYear: function () {
-	// 		return param1[0];
-	// 	}
-	// }
-	let obj = {
-		def: {
-			year: param1[0],
-			mounth: param1[1],
-			day: param1[2],
-			hour: param2[0],
-			minutes: param2[1],
+	// 	years: Number(param1[0]),
+	// 	mounths: Number(param1[1]),
+	// 	days: Number(param1[2]),
+	// 	hours: Number(param2[0]),
+	// 	minutes: Number(param2[1]),
+	// };
+
+	Object.defineProperties(data, {
+		'add': {
+			value: function (num, param) {
+				if (param === 'years')
+					data.setFullYear(data.getFullYear() + num);
+				if (param === 'months')
+					data.setMonth(data.getMonth() + num);
+				if (param === 'days')
+					data.setDate(data.getDate() + num);
+				if (param === 'hours')
+					data.setHours(data.getHours() + num);
+				if (param === 'minutes')
+					data.setMinutes(data.getMinutes() + num);
+				console.log(data);
+				return data;
+				// return data.toLocaleDateString("ru").split(".").reverse().join("-") +
+				// 	" " + data.toTimeString().split(" ")[0].slice(0,5);
+				// return data.getFullYear() + "-" + data.getMonth() + "-" + data.getDate() +
+				// 	" " + data.getHours() + ":" + data.getMinutes();
+			}
 		},
-		add: function () {
-			console.log(str);
-			return this;
+		'subtract': {
+			value: function (num, param) {
+				if (param === 'years')
+					data.setFullYear(data.getFullYear() - num);
+				if (param === 'months')
+					data.setMonth(data.getMonth() - num);
+				if (param === 'days')
+					data.setDate(data.getDate() - num);
+				if (param === 'hours')
+					data.setHours(data.getHours() - num);
+				if (param === 'minutes')
+					data.setMinutes(data.getMinutes() - num);
+				console.log(data);
+				return data;
+			}
+		},
+		'valueOf': {
+			value: function () {
+				return data.toLocaleDateString("ru").split(".").reverse().join("-") +
+				" " + data.toTimeString().split(" ")[0].slice(0, 5)
+			}
 		}
-	};
-	console.log(obj.def);
-	return obj.def;
-	// console.log(obj);
-	// Object.defineProperty(obj, 'add', {
-	// 	add: function () {
-	// 		console.log("haha");
-	// 	}
-	// })
+	});
+	return data;
 }
 
-let time = new Time('2017-05-16 13:45');
+let time = Time('2017-05-16 13:45');
 
-
-time.add();
+console.log(time.add(24, 'hours').subtract(1, 'months').add(3, 'days').add(15, 'minutes'));
+console.log(time.valueOf());
