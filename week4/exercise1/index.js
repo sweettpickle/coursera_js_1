4bbb0364453fded8eef6e72cc85c6e3feb23c9eb
sweettpickle <sweettpickle@gmail.com>
2020-02-19 09:00:14 +0300

    week 4

---
+++ week4/exercise1/index.js
@@ -0,0 +1,77 @@
+// Коллекция данных
+var friends = [
+    {
+        name: 'Сэм',
+        gender: 'Мужской',
+        email: 'luisazamora@example.com',
+        favoriteFruit: 'Картофель'
+    },
+    {
+        name: 'Эмили',
+        gender: 'Женский',
+        email: 'example@example.com',
+        favoriteFruit: 'Яблоко'
+    },
+    {
+        name: 'Мэт',
+        gender: 'Мужской',
+        email: 'danamcgee@example.com',
+        favoriteFruit: 'Яблоко'
+    },
+    {
+        name: 'Брэд',
+        gender: 'Мужской',
+        email: 'newtonwilliams@example.com',
+        favoriteFruit: 'Банан'
+    },
+    {
+        name: 'Шерри',
+        gender: 'Женский',
+        email: 'danamcgee@example.com',
+        favoriteFruit: 'Картофель'
+    },
+    {
+        name: 'Керри',
+        gender: 'Женский',
+        email: 'danamcgee@example.com',
+        favoriteFruit: 'Апельсин'
+    },
+    {
+        name: 'Стелла',
+        gender: 'Женский',
+        email: 'waltersguzman@example.com',
+        favoriteFruit: 'Картофель'
+    }
+];
+
+/**
+ * @param {Array} collection
+ * @params {Function[]} – Функции для запроса
+ * @returns {Array}
+ */
+function query(collection) {
+
+}
+
+/**
+ * @params {String[]}
+ */
+function select() {
+    // var fields =
+
+    // return ['select', fields];
+}
+
+/**
+ * @param {String} property – Свойство для фильтрации
+ * @param {Array} values – Массив разрешённых значений
+ */
+function filterIn(property, values) {
+
+}
+
+// module.exports = {
+//     query: query,
+//     select: select,
+//     filterIn: filterIn
+// };
